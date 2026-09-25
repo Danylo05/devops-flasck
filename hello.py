540bf042d390b7428dc1c6ce4e3b4782ca84f22d
Leslie Kaspruk <C24743569@mytudublin.ie>
2026-09-25 11:28:03 +0000

    CONAS ATA TU

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask (__name__)
 @app.route('/')
 def say_hello():
      return  '''
-     <p>Heelo, World, I am a Flask  app!<p>  
+     <p>CONAS ATA TU?<p>  
      <p><a href="/about">About this application</a><p>
       '''
 
